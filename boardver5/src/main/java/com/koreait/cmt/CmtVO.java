2081f07ae50a328d30a8fadbbdadea8d6cd87ad6
heck_evil <heck_evil@naver.com>
2021-05-17 22:45:33 +0900

    2021 05 17 댓글만드는중

---
+++ boardver5/src/main/java/com/koreait/cmt/CmtVO.java
@@ -0,0 +1,40 @@
+package com.koreait.cmt;
+
+public class CmtVO {
+	private int iboard;
+	private int icmt;
+	private String cmt;
+	private String unm;
+	private int iuser;
+	public int getIboard() {
+		return iboard;
+	}
+	public void setIboard(int iboard) {
+		this.iboard = iboard;
+	}
+	public int getIcmt() {
+		return icmt;
+	}
+	public void setIcmt(int icmt) {
+		this.icmt = icmt;
+	}
+	public String getCmt() {
+		return cmt;
+	}
+	public void setCmt(String cmt) {
+		this.cmt = cmt;
+	}
+	public String getUnm() {
+		return unm;
+	}
+	public void setUnm(String unm) {
+		this.unm = unm;
+	}
+	public int getIuser() {
+		return iuser;
+	}
+	public void setIuser(int iuser) {
+		this.iuser = iuser;
+	}
+	
+}
